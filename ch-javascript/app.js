7f713e58fe03fd40de64d2db20b2fa31bbbad3e1
Sandra Esmeralda Reyes Galván <poetry_madam_200@hotmail.com>
2025-07-27 05:23:27 -0600

    Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.

--- ch-javascript/app.js
+++ ch-javascript/app.js
@@ -10,11 +10,38 @@ push().
 Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
 */
 function agregarAmigo(){
-    if (amigos[amigos.length - 1] === "") {
+    if (document.getElementById("amigo").value === "") {
         alert("Por favor, inserte un nombre.");
     }else {
         amigos.push(document.getElementById("amigo").value);
         document.getElementById("amigo").value = "";
-        console.log(amigos);
+        mostrarAmigos(amigos);
+    }
+console.log("Amigo agregado: " + amigos[amigos.length - 1]);
+    console.log("Lista de amigos actualizada: " + amigos);
+}
+/*
+Crea una función que recorra el array amigos y agregue cada nombre 
+como un elemento <li> dentro de una lista HTML. 
+Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+
+Tareas específicas:
+
+Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+
+Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+*/
+function mostrarAmigos(amigos) {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+        console.log("Desplegando a: " +amigos[i]);
     }
 }
