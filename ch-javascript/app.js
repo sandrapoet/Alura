bb7ad6bbbe2d10f16c6ab7f5a719b6123a19d9c8
S@n <33068019+sandrapoet@users.noreply.github.com>
2025-07-27 04:55:40 -0600

    Función captura amigos

--- ch-javascript/app.js
+++ ch-javascript/app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+/*
+
+Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+*/
+function agregarAmigo(){
+    amigos.push(Document.getElementById(amigo).value);
+}
