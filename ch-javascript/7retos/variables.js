3bd4776423d43235684ea194a29ba1c24f70bc1e
S@n <33068019+sandrapoet@users.noreply.github.com>
2025-08-30 10:25:11 +0000

    priner reto completado

---
+++ ch-javascript/7retos/variables.js
@@ -0,0 +1,26 @@
+let numeroUno = 1;
+let stringUno = '1';
+let numeroTreinta = 30;
+let stringTreinta = '30';
+let numeroDiez = 10;
+let stringDiez = '10';
+
+if (numeroUno == stringUno) {
+    console.log('Las variables numeroUno y stringUno tienen el mismo valor, pero tipos diferentes');
+    } else {
+        console.log('Las variables numeroUno y stringUno no tienen el mismo valor');
+        }
+
+        if (numeroTreinta === stringTreinta) {
+            console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo');
+            } else {
+                console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo');
+                }
+
+                if (numeroDiez == stringDiez) {
+                    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+                    } else {
+                        console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+                        }
+
+//The code uses `==` for value comparison (loose equality) where the values are the same but types differ, and `===` for strict equality where both value and type must match
