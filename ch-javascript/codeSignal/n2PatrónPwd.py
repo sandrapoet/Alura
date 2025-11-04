c64c4d36975749646958a1a6b62f8a75229fb380
S@n <33068019+sandrapoet@users.noreply.github.com>
2025-11-04 10:38:03 -0600

    Update n2PatrónPwd.py

--- ch-javascript/codeSignal/n2PatrónPwd.py
+++ ch-javascript/codeSignal/n2PatrónPwd.py
@@ -26,3 +26,25 @@ def solution(password, pattern):
     }
     
     return all(checks[p](c) for c, p in zip(password, pattern))
+
+"""
+You are given two strings: pattern and source. The first string pattern contains only the symbols 0 and 1, and the second string source contains only lowercase English letters.
+– The pattern and substring are equal in length.
+– Where there is a 0 in the pattern, there is a vowel in the substring. 
+– Where there is a 1 in the pattern, there is a consonant in the substring. 
+"""
+def solution1(password, pattern):
+    if len(password) != len(pattern):
+        return False
+    
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    is_vowel = lambda c: c.lower() in vowels
+    
+    checks = {
+        '0': is_vowel,
+        '1': lambda c: not is_vowel(c)
+    }
+    
+    return all(checks[p](c) for c, p in zip(password, pattern))
+
+print(solution1("amazing","010"))
